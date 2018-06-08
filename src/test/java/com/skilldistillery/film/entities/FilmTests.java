60725992b68605b810fbbdc404b7e996b3a59a76
Wilson <wilson_lou@hotmail.co.uk>
2018-06-08 15:36:43 -0600

    first commit

---
+++ src/test/java/com/skilldistillery/film/entities/FilmTests.java
@@ -0,0 +1,24 @@
+package com.skilldistillery.film.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FilmTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
